344a2a23cfc0a92dd465c2272963be21643fabab
Lincoln Zhou <zhoulang731@gmail.com>
2025-03-28 19:59:05 +0100

    fuck my life

--- generate_temp.py
+++ generate_temp.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import re
 
+
+ph_codes = [
+    "A22.1", "A31", "A31.0", "A31.1", "A31.8", "A31.9", "A37.1", "A39.8",
+    "A43.0", "A48.1", "B25.0", "B97.2", "J10.0", "J11.0", "J12.0", "J12.1",
+    "J12.2", "J12.3", "J12.8", "J12.9", "J13", "J14", "J15", "J15.0", "J15.1",
+    "J15.2", "J15.3", "J15.4", "J15.5", "J15.6", "J15.7", "J15.8", "J15.9",
+    "J16", "J16.0", "J16.8", "J17", "J17.0", "J17.1", "J17.2", "J17.3", "J17.8",
+    "J18", "J18.0", "J18.1", "J18.8", "J18.9", "J22", "J84.9", "J85.1", "J86.0",
+    "J86.9", "J96.0", "J96.9", "J98.8", "U07.1"
+]
+
+icd_file = pd.read_csv('icd10.csv')
+
+icd_file = icd_file[icd_file['HIV_indicator_HIVteam'].isin([0, 1])]
+icd_file = icd_file[icd_file['HIV_indicator_HIVteam'].isin(ph_codes)]
+
 files = [f'Datauitgifte_AwareHIV_deidentified_chunk{x}.csv' for x in range(1, 4)]
 dfs = [pd.read_csv(f, index_col=0) for f in files]
 
@@ -8,7 +24,9 @@ df = pd.concat(dfs)
 
 df['authored'] = pd.to_datetime(df['authored'])
 
-df_sorted = df.sort_values(by=['Pseudoniem', 'authored', df.index.name], ascending=[True, False, True])
+df['ln'] = range(len(df))
+
+df_sorted = df.sort_values(by=['Pseudoniem', 'authored', 'ln'], ascending=[True, False, True])
 
 df_sorted['section_text'] = df_sorted['section_text'].apply(lambda x: x if str(x).endswith('\n') else str(x) + '\n')
 
