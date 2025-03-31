058ab986ffe74938fe7354fcfe147f3a6421b151
Lincoln Zhou <zhoulang731@gmail.com>
2025-03-31 22:18:02 +0400

    idk

--- rule_decision.py
+++ rule_decision.py
@@ -1,9 +1,19 @@
 # Based on Appendix E, version 2.1 8-7-2024
 # No considerations have been given to exclusive conditions yet
+from typing import List
 
 defining_set = (23, 16, 4, 7, 6, 18, 14, 2, 31, 21, 5, 8, 28)
 
 non_defining_set = (12, 11, 19, 30, 15, 34, 33, 13, 17, 29, 20, 1, 25, 24, 32, 35, 36, 10, 3, 27, 9, 22, 26)
+
+
+def decision(indicators: List[int]) -> bool:
+    for indicator in indicators:
+        if indicator in defining_set:
+            return True
+
+    return False
+
 
 if __name__ == "__main__":
     print(len(defining_set) + len(non_defining_set))
